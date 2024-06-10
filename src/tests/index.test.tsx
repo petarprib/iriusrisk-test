d322259b8460ffd33658a624246f5a0f3ede05ea
petarprib <pribetic.petar@gmail.com>
2024-06-10 02:47:35 +0200

    Correct function rename

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import { starshipDetails } from '../mocks/starships.mocks';
 import StarshipCard from '../views/starships/components/starship-card.component';
-import { parseCrewValue } from '../views/starships/utils/starships.utils';
+import { formatValue } from '../views/starships/utils/starships.utils';
 
 describe('starship card', () => {
   it('renders the correct data', () => {
@@ -16,24 +16,24 @@ describe('starship card', () => {
   });
 });
 
-describe('parseCrewValue', () => {
+describe('formatValue', () => {
   it('should return Infinity when the value is "unknown"', () => {
-    const result = parseCrewValue('unknown');
+    const result = formatValue('unknown');
     expect(result).toBe(Infinity);
   });
 
   it('receives two numbers separated by a dash (all as a single string), removes any dashes and returns the average of the two values as a numeric type', () => {
-    const value = parseCrewValue('20-100');
+    const value = formatValue('20-100');
     expect(value).toBe(60);
   });
 
   it('receives a number represented as a string, removes any commas and returns the number as a numeric type', () => {
-    const value = parseCrewValue('123,456');
+    const value = formatValue('123,456');
     expect(value).toBe(123456);
   });
 
   it('received a stringified number and returns that number as a numeric type', () => {
-    const value = parseCrewValue('123');
+    const value = formatValue('123');
     expect(value).toBe(123);
   });
 });
